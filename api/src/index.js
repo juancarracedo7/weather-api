f93bf97e8f8a563ba77b5d57432b201cdf6409d6
Juan Cruz Carracedo <juann.carracedo7@gmail.com>
2023-07-09 13:16:23 -0300

    new variable

--- api/src/index.js
+++ api/src/index.js
@@ -27,7 +27,7 @@ async function startServer() {
     console.log("Conexión exitosa a la base de datos");
     await connect();
     app.listen(DB_PORT, () => {
-      console.log("Servidor en ejecución en el puerto 3001");
+      console.log(`Servidor corriendo en el puerto ${DB_PORT}`);
     });
   } catch (error) {
     console.error("Error al conectar a la base de datos:", error);
